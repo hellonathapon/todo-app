0eb040c6fd6a80c141f337eede2526c9cfb87b1f
hellonathapon <hellonathapon@gmail.com>
2021-03-09 15:51:02 +0700

    Add CORS middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const port = process.env.PORT || 5000;
 const db = require('./config/db');
 require('dotenv').config();
 
+// CORS middleware
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // route
 app.use('/', require('./routes')(db))
 
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = function(db){
-    //#all todos
     router.get('/', (req, res) => {
         db.query(' SELECT * FROM todo ', (err, rows) => {
             if(err){
                 throw err
             }
-            console.log(rows)
+            res.send(rows)
+
         })
-        res.send({hi: ":)"})
     })
 
     router.post('/add', (req, res) => {
